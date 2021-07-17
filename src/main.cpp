7d9848c2a1caf93b6479b690764db479a2060346
Aaron Christiansen <aaronc20000@gmail.com>
2021-07-17 19:52:40 +0100

    Rotate display, add button log

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #define USE_DMA_TO_TFT
 #define COLOR_DEPTH 16
 
-#define IWIDTH  240
-#define IHEIGHT 320
+#define IWIDTH  320
+#define IHEIGHT 240
 
 #define CUBE_SIZE 200
 
@@ -34,56 +34,36 @@ void setup() {
   tft.init();
   tft.fillScreen(TFT_BLACK);
   tft.initDMA();
+  tft.setRotation(3);
 
   // Set up sprite
   sprite.setColorDepth(COLOR_DEPTH);
   spriteData = (uint16_t*)sprite.createSprite(IWIDTH, IHEIGHT);
-  sprite.setTextColor(TFT_BLACK);
+  sprite.setTextColor(TFT_WHITE);
   sprite.setTextDatum(MC_DATUM);
-
-  sleep_ms(3000);
-  Serial.println("Go!");
 }
+
+int y = 10;
 
 void loop() {
   // Grab exclusive use of the SPI bus
   tft.startWrite();
-
-  // Draw something
-  /*
-  sprite.fillScreen(0);
-  for (int i = 0; i < 128; i++) {
-    sprite.fillRect(random(IWIDTH), random(IHEIGHT), 20, 20, random(INT16_MAX));
-  }
-  tft.pushImageDMA(0, 0, IWIDTH, IHEIGHT, spriteData);
-  */
-
-  //                     X1, X2,  Y1,  Y2
-  int startPoints[4] = { 40, 60,  10,  80 };
-  int endPoints[4]   = { 10, 120, 100, 200 };
-
-  Animate<4> ease(startPoints, endPoints, 20, Easing::EASE_OUT);
-
-  while (ease.tick()) {
-    sprite.fillScreen(0);
-    sprite.fillRect(
-      ease.currentValue[0],
-      ease.currentValue[2],
-      ease.currentValue[1] - ease.currentValue[0],
-      ease.currentValue[3] - ease.currentValue[2],
-      0xFFFFFFFF
-    );
-    tft.pushImageDMA(0, 0, IWIDTH, IHEIGHT, spriteData);
-    sleep_ms(10);
-  }
-  
-  // Release bus
-  tft.endWrite();
 
   uint8_t r, c;
   if (buttons.getButton(r, c)) {
     Serial.println(r);
     Serial.println(c);
     Serial.println("-----");
+    sprite.drawNumber(r, 10, y);
+    sprite.drawNumber(c, 30, y);
+    tft.pushImageDMA(0, 0, IWIDTH, IHEIGHT, spriteData);
+
+    Serial.println(y);
+    y += 10;
   }
+
+  sleep_ms(10);
+  
+  // Release bus
+  tft.endWrite();
 }
