ef612d534d5bc15fbadccd96f80da5ecf30f8885
Aaron Christiansen <aaronc20000@gmail.com>
2021-08-03 20:47:15 +0100

    Add renderer and panic bridge

--- rbop_bridge/src/lib.rs
+++ rbop_bridge/src/lib.rs
@@ -6,8 +6,8 @@ extern crate alloc;
 mod c_allocator;
 
 use core::panic::PanicInfo;
-use alloc::{vec, boxed::Box};
-use rbop::{UnstructuredNodeList, nav::NavPath, node::unstructured::UnstructuredNodeRoot};
+use alloc::{boxed::Box, format, vec::Vec, vec};
+use rbop::{Token, UnstructuredNode, UnstructuredNodeList, nav::NavPath, node::unstructured::UnstructuredNodeRoot, render::{Area, CalculatedPoint, Glyph, Renderer}};
 use c_allocator::CAllocator;
 
 #[global_allocator]
@@ -18,15 +18,107 @@ pub struct RbopContext {
     nav_path: NavPath,
 }
 
-#[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
-    loop {}
+#[repr(C)]
+pub struct RbopRendererInterface {
+    clear: extern "C" fn() -> (),
+    draw_char: extern "C" fn(x: u64, y: u64, character: u8),
+    draw_line: extern "C" fn(x1: u64, y1: u64, x2: u64, y2: u64),
 }
 
+impl Renderer for RbopRendererInterface {
+    fn size(&mut self, glyph: Glyph) -> Area {
+        let text_character_size = Area { height: 8, width: 6 };
+
+        match glyph {
+            Glyph::Cursor { height } => Area { height, width: 1 },
+            Glyph::Digit { .. } => text_character_size,
+            Glyph::Add => text_character_size,
+            Glyph::Subtract => text_character_size,
+            Glyph::Multiply => text_character_size,
+            Glyph::Divide => text_character_size,
+            Glyph::Fraction { inner_width } => Area { height: 1, width: inner_width },
+
+            _ => unimplemented!(),
+        }
+    }
+
+    fn init(&mut self, size: Area) {
+        (self.clear)()
+    }
+
+    fn draw(&mut self, glyph: Glyph, point: CalculatedPoint) {
+        match glyph {
+            Glyph::Digit { number } => (self.draw_char)(point.x, point.y, number + ('0' as u8)),
+            Glyph::Add => (self.draw_char)(point.x, point.y, '+' as u8),
+            Glyph::Subtract => (self.draw_char)(point.x, point.y, '-' as u8),
+            Glyph::Multiply => (self.draw_char)(point.x, point.y, '*' as u8),
+            Glyph::Divide => (self.draw_char)(point.x, point.y, '/' as u8),
+
+            Glyph::Fraction { inner_width } =>
+                (self.draw_line)(point.x, point.y, point.x + inner_width, point.y),
+
+            Glyph::Cursor { height } =>
+                (self.draw_line)(point.x, point.y, point.x, point.y + height),
+
+            _ => todo!(),
+        }
+    }
+}
+
+static mut PANIC_HANDLER_FN: Option<extern "C" fn(*const u8) -> ()> = None;
+
+/// Sets a function to be called when a Rust panic occurs.
+#[no_mangle]
+pub extern "C" fn rbop_set_panic_handler(func: extern "C" fn(*const u8) -> ()) {
+    unsafe {
+        PANIC_HANDLER_FN = Some(func);
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    unsafe {
+        let message = format!("{}", info);
+        let mut message_bytes = message.as_bytes().iter().cloned().collect::<Vec<_>>();
+        message_bytes.push(0);
+
+        (PANIC_HANDLER_FN.unwrap())(message_bytes.as_ptr());
+        loop {}
+    }
+}
+
+/// Allocates and returns a new rbop context.
 #[no_mangle]
 pub extern "C" fn rbop_new() -> *mut RbopContext {
     Box::into_raw(Box::new(RbopContext {
         root: UnstructuredNodeRoot { root: UnstructuredNodeList { items: vec![] } },
-        nav_path: NavPath::new(vec![]),
+        nav_path: NavPath::new(vec![0]),
     }))
+}
+
+/// Frees an allocated rbop context.
+#[no_mangle]
+pub extern "C" fn rbop_free(ctx: *mut RbopContext) {
+    // From the FFI Omnibus
+    // To "free" a Box which we grabbed as a raw pointer, we just need to get the Box back
+    // Since the Box then goes out-of-scope, it is dropped and the memory is freed
+    // Thanks, Rust!
+    unsafe { Box::from_raw(ctx); }
+}
+
+#[no_mangle]
+pub extern "C" fn rbop_foo(ctx: *mut RbopContext) {
+    unsafe {
+        let ctx = ctx.as_mut().unwrap();
+        ctx.root.insert(&mut ctx.nav_path, UnstructuredNode::Token(Token::Digit(3)));
+    }
+}
+
+/// Renders an rbop context onto the screen.
+#[no_mangle]
+pub extern "C" fn rbop_render(ctx: *mut RbopContext, renderer: *mut RbopRendererInterface) {
+    unsafe {
+        let ctx = ctx.as_mut().unwrap();
+        renderer.as_mut().unwrap().draw_all(&ctx.root, Some(&mut ctx.nav_path.to_navigator()));
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,6 @@ extern "C" {
 #define IWIDTH  320
 #define IHEIGHT 240
 
-#define CUBE_SIZE 200
-
 TFT_eSPI tft = TFT_eSPI();
 
 arduino::MbedI2C i2c(I2C_SDA_PIN, I2C_SCL_PIN);
@@ -27,69 +25,34 @@ PCF8574 rowPcf(i2c, I2C_EXPANDER_ADDRESS_2);
 
 ButtonMatrix buttons(rowPcf, colPcf);
 
-#define GRID_ITEMS 6
-#define GRID_ROWS 2
-#define GRID_COLS 3
+TFT_eSprite sprite(&tft);
+uint16_t *spriteData;
 
-#define GRID_SPRITE_WIDTH (IWIDTH / GRID_COLS)
-#define GRID_SPRITE_HEIGHT (IHEIGHT / GRID_ROWS)
-
-#define GRID_UNSELECTED_PADDING 12
-#define GRID_SELECTED_PADDING 2
-
-// x1, y1, x2, y2 
-int grid_animate_start[4] = {
-  GRID_UNSELECTED_PADDING,
-  GRID_UNSELECTED_PADDING,
-  GRID_SPRITE_WIDTH - GRID_UNSELECTED_PADDING * 2,
-  GRID_SPRITE_HEIGHT - GRID_UNSELECTED_PADDING * 2
-};
-int grid_animate_end[4] = {
-  GRID_SELECTED_PADDING,
-  GRID_SELECTED_PADDING,
-  GRID_SPRITE_WIDTH - GRID_SELECTED_PADDING * 2,
-  GRID_SPRITE_HEIGHT - GRID_SELECTED_PADDING * 2
-};
-
-#define GRID_ANIMATE_TIME_FRAME 20
-
-TFT_eSprite menuItemSprites[GRID_ITEMS] = {
-  TFT_eSprite(&tft),
-  TFT_eSprite(&tft),
-  TFT_eSprite(&tft),
-  TFT_eSprite(&tft),
-  TFT_eSprite(&tft),
-  TFT_eSprite(&tft),
-};
-uint16_t* menuItemSpriteData[GRID_ITEMS];
-uint8_t menuItemSelection = 0;
-Animate<4>* menuItemSelectionAnimation;
-
-void drawUnselected(uint8_t idx) {
-  menuItemSprites[idx].fillScreen(0);
-  menuItemSprites[idx].fillRect(
-    GRID_UNSELECTED_PADDING,
-    GRID_UNSELECTED_PADDING,
-    GRID_SPRITE_WIDTH - GRID_UNSELECTED_PADDING * 2,
-    GRID_SPRITE_HEIGHT - GRID_UNSELECTED_PADDING * 2,
-    0xFFFFFFFF
-  );
+void rbopRendererClear() {
+  sprite.fillScreen(TFT_BLACK);
 }
 
-void resetSelectedAnimation() {
-  // Restore previously selected item
-  drawUnselected(menuItemSelection);
+void rbopRendererDrawLine(uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2) {
+  sprite.drawLine(x1, y1, x2, y2, TFT_WHITE);
+}
 
-  // Set up animation
-  menuItemSelectionAnimation = new Animate<4>(
-    grid_animate_start,
-    grid_animate_end,
-    GRID_ANIMATE_TIME_FRAME,
-    Easing::EASE_OUT
-  );
+void rbopRendererDrawChar(uint64_t x, uint64_t y, uint8_t c) {
+  sprite.setCursor(x, y);
+  sprite.print((char)c);
+}
+
+void rbopPanicHandler(const uint8_t *message) {
+  sprite.setCursor(0, 0);
+  sprite.println("PANIC!");
+  sprite.println((const char*)message);
+  tft.startWrite();
+  tft.pushImageDMA(10, 10, 100, 100, spriteData);
+  tft.endWrite();
 }
 
 void setup() {
+  rbop_set_panic_handler(rbopPanicHandler);
+
   RbopContext *ctx = rbop_new();
 
   Serial.begin(115200);
@@ -101,62 +64,23 @@ void setup() {
   tft.initDMA();
   tft.setRotation(3);
 
-  // Set up sprite
-  for (int i = 0; i < GRID_ITEMS; i++) {
-    menuItemSprites[i].setColorDepth(COLOR_DEPTH);
-    menuItemSpriteData[i] = (uint16_t*)menuItemSprites[i].createSprite(
-      GRID_SPRITE_WIDTH, GRID_SPRITE_HEIGHT
-    );
-    menuItemSprites[i].setTextColor(TFT_WHITE);
-    menuItemSprites[i].setTextDatum(MC_DATUM);
+  sprite.setColorDepth(COLOR_DEPTH);
+  spriteData = (uint16_t*)sprite.createSprite(100, 100);
+  sprite.setTextColor(TFT_WHITE);
+  sprite.setTextDatum(MC_DATUM);
 
-    drawUnselected(i);
-  }
+  RbopRendererInterface renderer = {
+    .clear = rbopRendererClear,
+    .draw_char = rbopRendererDrawChar,
+    .draw_line = rbopRendererDrawLine,
+  };
 
-  resetSelectedAnimation();
-}
+  rbop_foo(ctx);
+  rbop_render(ctx, &renderer);
 
-int y = 10;
-
-void loop() {
-  // Grab exclusive use of the SPI bus
   tft.startWrite();
-
-  for (int row = 0; row < GRID_ROWS; row++) {
-    for (int col = 0; col < GRID_COLS; col++) {
-      int idx = row * GRID_COLS + col;
-      if (idx == menuItemSelection) {
-        menuItemSelectionAnimation->tick();
-        int *cv = menuItemSelectionAnimation->currentValue;
-
-        menuItemSprites[idx].fillScreen(0);
-        menuItemSprites[idx].fillRect(
-          cv[0],
-          cv[1],
-          cv[0] + cv[2],
-          cv[1] + cv[3],
-          0xFFFFFFFF
-        );
-      }
-
-      tft.pushImageDMA(
-        col * GRID_SPRITE_WIDTH,
-        row * GRID_SPRITE_HEIGHT,
-        GRID_SPRITE_WIDTH,
-        GRID_SPRITE_HEIGHT,
-        menuItemSpriteData[idx]
-      );
-    }
-  }
-  
-  uint8_t r, c;
-  ButtonEvent evt;
-  if (buttons.waitForEvent(r, c, evt) && evt == ButtonEvent::PRESS) {
-    resetSelectedAnimation();
-    menuItemSelection++;
-    menuItemSelection %= 6;
-  }
-
-  // Release bus
+  tft.pushImageDMA(10, 10, 100, 100, spriteData);
   tft.endWrite();
 }
+
+void loop() {}
