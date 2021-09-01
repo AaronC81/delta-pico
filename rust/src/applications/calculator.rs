5b24a5e5926b0ba13b49e45f8fef645377b0a4a6
Aaron Christiansen <aaronc20000@gmail.com>
2021-09-01 23:32:35 +0100

    Improve look of calculator app

--- rust/src/applications/calculator.rs
+++ rust/src/applications/calculator.rs
@@ -38,11 +38,12 @@ impl Application for CalculatorApplication {
         // Draw
         framework().rbop_location_x = PADDING;
         framework().rbop_location_y = PADDING;
-        framework().draw_all(
+        let layout = framework().draw_all(
             &self.rbop_ctx.root, 
             Some(&mut self.rbop_ctx.nav_path.to_navigator()),
             self.rbop_ctx.viewport.as_ref(),
         );
+        let input_end_y = layout.area(framework()).height + PADDING;
         
         // Evaluate
         let result = if let Ok(structured) = self.rbop_ctx.root.upgrade() {
@@ -55,17 +56,30 @@ impl Application for CalculatorApplication {
             None
         };
 
+        // Draw a line
+        (framework().display.draw_line)(
+            PADDING as i64, (input_end_y + PADDING) as i64,
+            (framework().display.width - PADDING) as i64, (input_end_y + PADDING) as i64,
+            colour::GREY
+        );
+
         // Write result
         if let Some(result) = result {
-            let result_str = result.to_string();
-            let mut result_chars = result_str.as_bytes().to_vec();
-            result_chars.push(0);
+            // Convert decimal to string and truncate
+            let mut result_str = result.to_string();
+            if result_str.len() > 15 {
+                result_str = result_str[0..15].to_string();
+            }
 
+            // Calculate length for right-alignment
+            let (result_str_len, _) = framework().display.string_size(&result_str);
+
+            // Write text
             (framework().display.set_cursor)(
-                0,
-                (framework().display.height - PADDING * 2 - 30) as i64
+                (framework().display.width - PADDING) as i64 - result_str_len,
+                (input_end_y + PADDING * 2) as i64
             );
-            (framework().display.print)(result_chars.as_ptr());
+            framework().display.print(result_str);
         }
 
         // Push to screen
