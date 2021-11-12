bd84c8a9e6dd363013b719cf1a092515ccb2533a
Aaron Christiansen <aaronc20000@gmail.com>
2021-11-12 18:37:02 +0000

    Remove duplicate apps

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -56,10 +56,6 @@ pub extern "C" fn delta_pico_main() {
     os().application_list.add::<applications::graph::GraphApplication>();
     os().application_list.add::<applications::about::AboutApplication>();
     os().application_list.add::<applications::numbers_game::NumbersGame>();
-    os().application_list.add::<applications::graph::GraphApplication>();
-    os().application_list.add::<applications::about::AboutApplication>();
-    os().application_list.add::<applications::graph::GraphApplication>();
-    os().application_list.add::<applications::about::AboutApplication>();
     os().application_list.add::<applications::storage::StorageApplication>();
     os().application_list.add::<applications::bootloader::BootloaderApplication>();
 
