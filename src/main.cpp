4f505e968a392a2a617d735b16290ebd75e10207
Aaron Christiansen <aaronc20000@gmail.com>
2021-12-24 16:54:42 +0000

    Remove unused TinyUSB callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -240,33 +240,3 @@ int main() {
   delta_pico_set_framework(&framework_interface);
   delta_pico_main();
 }
-
-
-// Invoked when device is mounted
-void tud_mount_cb(void)
-{
-}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-}
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-}
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-}
