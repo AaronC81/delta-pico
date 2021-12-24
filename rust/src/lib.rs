3bb3707b4f531e9c6a90b0dbd4999372de382d66
Aaron Christiansen <aaronc20000@gmail.com>
2021-12-24 21:56:19 +0000

    Show a splash screen while loading storage

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -86,6 +86,11 @@ pub extern "C" fn delta_pico_main() {
         os().ui_text_dialog("Unable to communicate with storage.");
     }
 
+    // Show a splash screen while we load storage
+    framework().display.fill_screen(Colour::BLACK);
+    framework().display.draw_bitmap(60, 80, "splash");
+    framework().display.draw();
+
     // Temporary
     let mut fat = os().filesystem.fat.read_all().unwrap();
     framework().usb_mass_storage.fat12_filesystem = fat.as_mut_ptr();    
