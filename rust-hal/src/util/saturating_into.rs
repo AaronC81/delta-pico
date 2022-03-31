442ddc8dbbb2989e6d7ea66f54bd611b8c773f72
Aaron Christiansen <aaronc20000@gmail.com>
2022-03-31 17:39:09 +0100

    More SaturatingInto implementations

--- rust-hal/src/util/saturating_into.rs
+++ rust-hal/src/util/saturating_into.rs
@@ -25,3 +25,26 @@ impl SaturatingInto<u16> for i64 {
     }
 }
 
+impl SaturatingInto<u32> for i64 {
+    fn saturating_into(self) -> u32 {
+        if self > u32::MAX.into() { return u32::MAX }
+        if self < 0 { return 0 }
+        self as u32
+    }
+}
+
+impl SaturatingInto<i32> for i64 {
+    fn saturating_into(self) -> i32 {
+        if self > i32::MAX.into() { return i32::MAX }
+        if self < i32::MIN.into() { return i32::MIN }
+        self as i32
+    }
+}
+
+impl SaturatingInto<i16> for i32 {
+    fn saturating_into(self) -> i16 {
+        if self > i16::MAX.into() { return i16::MAX }
+        if self < i16::MIN.into() { return i16::MIN }
+        self as i16
+    }
+}
