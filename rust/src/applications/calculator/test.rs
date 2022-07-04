625427ccf25b626d983428b00351940951454aa0
Aaron Christiansen <aaronc20000@gmail.com>
2022-07-04 01:05:23 +0100

    Add catalog test

--- rust/src/applications/calculator/test.rs
+++ rust/src/applications/calculator/test.rs
@@ -1,4 +1,5 @@
-use rbop::Number;
+use num_traits::One;
+use rbop::{Number, node::structured::AngleUnit};
 
 use crate::{interface::{ApplicationFramework, ButtonInput}, tests, operating_system::OSInput, filesystem::CalculationResult};
 
@@ -32,5 +33,24 @@ pub fn test<F: ApplicationFramework>(app: &mut CalculatorApplication<F>) {
     assert!(matches!(
         app.calculations[app.calculations.len() - 2].result,
         CalculationResult::Ok(Number::Rational(5, 3))
+    ));
+
+    // Catalog
+    app.os.filesystem.settings.values.angle_unit = AngleUnit::Degree;
+    tests::press(app, &[
+        // Select sine
+        OSInput::Button(ButtonInput::List),
+        OSInput::Button(ButtonInput::Exe),
+        OSInput::Button(ButtonInput::MoveDown),
+        OSInput::Button(ButtonInput::Exe),
+
+        // Enter 90 degrees
+        OSInput::Button(ButtonInput::Digit(9)),
+        OSInput::Button(ButtonInput::Digit(0)),
+        OSInput::Button(ButtonInput::Exe),
+    ]);
+    assert!(matches!(
+        app.calculations[app.calculations.len() - 2].result,
+        CalculationResult::Ok(Number::Decimal(d)) if d.is_one()
     ));
 }
