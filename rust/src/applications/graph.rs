10fc96d1aba84c432aafaeba29ca30a7df26dadd
Aaron Christiansen <aaronc20000@gmail.com>
2022-07-05 22:01:18 +0100

    Explicitly recalculate graph values, and store in `Plot`s
    Gives us room to do optimisations by only recalculating slices

--- rust/src/applications/graph.rs
+++ rust/src/applications/graph.rs
@@ -1,5 +1,5 @@
-use alloc::vec::Vec;
-use rbop::{Number, StructuredNode, node::{unstructured::{Upgradable, UnstructuredNodeRoot}, structured::EvaluationSettings}};
+use alloc::{vec::Vec, string::ToString};
+use rbop::{Number, StructuredNode, node::{unstructured::{Upgradable, UnstructuredNodeRoot}, structured::EvaluationSettings}, error::{MathsError, NodeError}};
 use rust_decimal::prelude::{One, ToPrimitive, Zero};
 
 use crate::{interface::{Colour, ApplicationFramework, ButtonInput, DISPLAY_WIDTH, DISPLAY_HEIGHT}, operating_system::{OSInput, OperatingSystem, os_accessor, OperatingSystemPointer}};
@@ -74,6 +74,24 @@ impl ViewWindow {
 
 struct Plot {
     root: UnstructuredNodeRoot,
+    y_values: Vec<Result<Number, MathsError>>,
+}
+
+impl Plot {
+    fn recalculate_values(&mut self, view: &ViewWindow, settings: &EvaluationSettings) -> Result<(), NodeError> {
+        let upgraded = self.root.upgrade()?;
+        let func = |x| {
+            let sn_clone = upgraded.substitute_variable(
+                'x',
+                &StructuredNode::Number(x)
+            );
+            sn_clone.evaluate(settings)
+        };
+        self.y_values = view.x_coords_on_screen()
+            .iter().map(|i| func(*i)).collect::<Vec<_>>();
+
+        Ok(())
+    }
 }
 
 pub struct GraphApplication<F: ApplicationFramework + 'static> {
@@ -108,15 +126,35 @@ impl<F: ApplicationFramework> Application for GraphApplication<F> {
         // Poll for input
         if let Some(input) = self.os_mut().input() {
             let ten = Number::from(10);
+            let mut recalculation_required = false;
             match input {
-                OSInput::Button(ButtonInput::MoveLeft) => self.view_window.pan_x += ten,
-                OSInput::Button(ButtonInput::MoveRight) => self.view_window.pan_x -= ten,
-                OSInput::Button(ButtonInput::MoveUp) => self.view_window.pan_y -= ten,
-                OSInput::Button(ButtonInput::MoveDown) => self.view_window.pan_y += ten,
+                OSInput::Button(ButtonInput::MoveLeft) => {
+                    self.view_window.pan_x += ten;
+                    recalculation_required = true;
+                },
+                OSInput::Button(ButtonInput::MoveRight) => {
+                    self.view_window.pan_x -= ten;
+                    recalculation_required = true;
+                }
+                OSInput::Button(ButtonInput::MoveUp) => {
+                    self.view_window.pan_y -= ten;
+                    recalculation_required = true;
+                }
+                OSInput::Button(ButtonInput::MoveDown) => {
+                    self.view_window.pan_y += ten;
+                    recalculation_required = true;
+                }
 
                 OSInput::Button(ButtonInput::List) => self.open_menu(),
 
                 _ => (),
+            }
+
+            if recalculation_required {
+                let settings = self.settings();
+                for plot in &mut self.plots {
+                    plot.recalculate_values(&self.view_window, &settings).expect("unchanged plot encountered error");
+                }
             }
         }
     }
@@ -132,37 +170,22 @@ impl<F: ApplicationFramework> GraphApplication<F> {
         self.os_mut().display_sprite.draw_line(0, y_axis, DISPLAY_WIDTH as i16, y_axis, Colour::BLUE);
 
         // Upgrade, substitute, and draw each graph
-        for graph in &self.plots {
-            if let Ok(sn) = graph.root.upgrade() {
-                let func = |x| {
-                    let sn_clone = sn.substitute_variable(
-                        'x',
-                        &StructuredNode::Number(x)
-                    );
-                    sn_clone.evaluate(&EvaluationSettings {
-                        use_floats: true,
-                        ..self.os().filesystem.settings.evaluation_settings()
-                    })
-                };
-                let values = self.view_window.x_coords_on_screen()
-                    .iter().map(|i| func(*i)).collect::<Vec<_>>();
-        
-                let mut next_y_screen = self.view_window.y_to_screen(values[0].clone().unwrap_or(Number::zero()));
-                for this_x in 0..(values.len() - 1) {
-                    let next_x = this_x + 1;
+        for plot in &self.plots {
+            let mut next_y_screen = self.view_window.y_to_screen(plot.y_values[0].clone().unwrap_or(Number::zero()));
+            for this_x in 0..(plot.y_values.len() - 1) {
+                let next_x = this_x + 1;
 
-                    values[this_x].as_ref().unwrap();
-                    if let Ok(this_y) = values[this_x] {
-                        let next_y = values[next_x].as_ref().unwrap_or(&this_y);
-                        let this_y_screen = next_y_screen;
-                        next_y_screen = self.view_window.y_to_screen(*next_y);
-            
-                        self.os_mut().display_sprite.draw_line(
-                            this_x as i16, this_y_screen,
-                            next_x as i16, next_y_screen,
-                            Colour::WHITE
-                        );
-                    }
+                plot.y_values[this_x].as_ref().unwrap();
+                if let Ok(this_y) = plot.y_values[this_x] {
+                    let next_y = plot.y_values[next_x].as_ref().unwrap_or(&this_y);
+                    let this_y_screen = next_y_screen;
+                    next_y_screen = self.view_window.y_to_screen(*next_y);
+        
+                    self.os_mut().display_sprite.draw_line(
+                        this_x as i16, this_y_screen,
+                        next_x as i16, next_y_screen,
+                        Colour::WHITE
+                    );
                 }
             }
         }
@@ -180,8 +203,20 @@ impl<F: ApplicationFramework> GraphApplication<F> {
 
         match idx {
             Some(0) => {
-                let root = self.os_mut().ui_input_expression("y =", None);
-                self.plots.push(Plot { root });
+                // Take input repeatedly until we get something which calculates valid values
+                let mut root = None;
+                let plot = loop {
+                    root = Some(self.os_mut().ui_input_expression("y =", root));
+                    let mut plot = Plot { root: root.clone().unwrap(), y_values: Vec::new() };
+                    match plot.recalculate_values(&self.view_window, &self.settings()) {
+                        Ok(_) => break plot,
+                        Err(e) => {
+                            self.os_mut().ui_text_dialog(&e.to_string());
+                            self.draw();
+                        }
+                    }
+                };
+                self.plots.push(plot);
             }
 
             Some(1) => {
@@ -212,6 +247,13 @@ impl<F: ApplicationFramework> GraphApplication<F> {
             },
             None => (),
             _ => unreachable!()
+        }
+    }
+
+    fn settings(&self) -> EvaluationSettings {
+        EvaluationSettings {
+            use_floats: true,
+            ..self.os().filesystem.settings.evaluation_settings()
         }
     }
 }
