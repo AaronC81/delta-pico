19210cf78727c7c83aa5ac595be665e3543fe064
Aaron Christiansen <aaronc20000@gmail.com>
2022-07-06 22:22:42 +0100

    Add dividers to context menu

--- rust/src/operating_system/context_menu.rs
+++ rust/src/operating_system/context_menu.rs
@@ -1,6 +1,6 @@
 use alloc::{string::String, vec::Vec, boxed::Box};
 
-use crate::{interface::{ApplicationFramework, DISPLAY_HEIGHT, DISPLAY_WIDTH, Colour, ShapeFill, ButtonInput}, operating_system::OSInput};
+use crate::{interface::{ApplicationFramework, DISPLAY_HEIGHT, DISPLAY_WIDTH, Colour, ShapeFill, ButtonInput}, operating_system::OSInput, graphics::Sprite};
 
 use super::{OperatingSystemPointer, SelectorMenu, SelectorMenuTickResult, SelectorMenuItem};
 
@@ -19,6 +19,30 @@ pub enum ContextMenuItem<T> {
         text: String,
         metadata: T,
     },
+    Divider,
+}
+
+impl<T> ContextMenuItem<T> {
+    pub fn height(&self) -> u16 {
+        match self {
+            Self::Text { .. } => 30,
+            Self::Divider => 8,
+        }
+    }
+
+    pub fn draw(&self, sprite: &mut Sprite, y: i16) {
+        match self {
+            Self::Text { text, .. } => sprite.print_at(10, y + 4, text),
+            Self::Divider => sprite.draw_line(5, y + 4, DISPLAY_WIDTH as i16 - 5, y + 4, Colour::WHITE),
+        }
+    }
+
+    pub fn selectable(&self) -> bool {
+        match self {
+            Self::Text { .. } => true,
+            Self::Divider => false,
+        }
+    }
 }
 
 impl<A> ContextMenuItem<Box<dyn FnOnce(&mut A)>> {
@@ -36,12 +60,14 @@ impl<T> SelectorMenuItem for ContextMenuItem<T> {
     fn inner(&self) -> &T {
         match self {
             Self::Text { metadata, .. } => metadata,
+            Self::Divider => panic!("tried to get inner of unselectable item"),
         }
     }
 
     fn into_inner(self) -> T {
         match self {
             Self::Text { metadata, .. } => metadata,
+            Self::Divider => panic!("tried to get inner of unselectable item"),
         }
     }
 }
@@ -69,28 +95,33 @@ where F: ApplicationFramework + 'static,
     fn into_items(self) -> Vec<Self::Item> { self.items }
 
     fn tick(&mut self) -> SelectorMenuTickResult {
-        const ITEM_GAP: i16 = 30;
+        let total_height: u16 = self.items.iter().map(|i| i.height()).sum();
 
         // Draw background
-        let mut y = DISPLAY_HEIGHT as i16 - ITEM_GAP * self.items.len() as i16 - 10;
+        let mut y = (DISPLAY_HEIGHT - total_height - 20) as i16;
         self.os.display_sprite.draw_rect(0, y, DISPLAY_WIDTH, 400, Colour::GREY, ShapeFill::Filled, 10);
         self.os.display_sprite.draw_rect(0, y, DISPLAY_WIDTH, 400, Colour::WHITE, ShapeFill::Hollow, 10);
 
         // Draw items
         y += 10;
         for (i, item) in self.items.iter().enumerate() {
+            // Calculate height of this item
+            let height = item.height();
+
+            // If it's selected, draw a background
             if i == self.selected_index {
                 let width = DISPLAY_WIDTH;
                 self.os.display_sprite.draw_rect(
-                    5, y as i16, width - 5 * 2, 25,
+                    5, y as i16, width - 5 * 2, height,
                     Colour::BLUE, ShapeFill::Filled, 7
                 );
             }
-            self.os.display_sprite.print_at(10, y + 4, match item {
-                ContextMenuItem::Text { text, .. } => &text,
-            });
+            
+            // Draw item
+            item.draw(&mut self.os.display_sprite, y);
 
-            y += ITEM_GAP;
+            // Move up by height
+            y += height as i16;
         }
 
         // Redraw
@@ -100,15 +131,23 @@ where F: ApplicationFramework + 'static,
         if let Some(btn) = self.os.input() {
             match btn {
                 OSInput::Button(ButtonInput::MoveUp) => {
-                    if self.selected_index == 0 {
-                        self.selected_index = self.items.len() - 1;
-                    } else {
-                        self.selected_index -= 1;
+                    loop {
+                        if self.selected_index == 0 {
+                            self.selected_index = self.items.len() - 1;
+                        } else {
+                            self.selected_index -= 1;
+                        }
+
+                        if self.items[self.selected_index].selectable() { break }
                     }
                 }
                 OSInput::Button(ButtonInput::MoveDown) => {
-                    self.selected_index += 1;
-                    self.selected_index %= self.items.len();
+                    loop {
+                        self.selected_index += 1;
+                        self.selected_index %= self.items.len();
+
+                        if self.items[self.selected_index].selectable() { break }
+                    }
                 }
                 OSInput::Button(ButtonInput::Exe) => {
                     return SelectorMenuTickResult::Selected;
